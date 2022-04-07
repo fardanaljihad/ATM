bc58ffca9f64d9aeb502c099543f0e9daf076f1c
fardantif21 <fardan.al.tif421@polban.ac.id>
2022-04-07 08:27:30 +0700

    menambahkan folder ATM

---
+++ atm/ATMMain.java
@@ -0,0 +1,9 @@
+package atm;
+
+public class ATMMain {
+    // main method creates and runs the ATM
+    public static void main(String[] args) {
+        ATM theATM = new ATM();    
+        theATM.run();
+    }
+} 
